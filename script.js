660b0034dcae3b06766142ddee51670831155ff5
Michel Medina Tena <124810680+micheee24@users.noreply.github.com>
2023-05-22 13:07:26 -0700

    Skills Update

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ window.onscroll = () => {
         };
     });
 };
+
+let number = document.getElementById("number1");
+let counter = 0; 
+setInterval(() => {
+    if(counter == 65){
+        clearInterval
+    }
+    else{
+    counter += 1;
+    number.innerHTML = counter + "%";
+    }
+}, 30);
