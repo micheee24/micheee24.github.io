6f6f4df44d3895ab507371e5d89e5c4fd606c7e0
Michel Medina Tena <124810680+micheee24@users.noreply.github.com>
2023-05-22 22:19:58 -0700

    Organized Sections

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.onscroll = () => {
     });
 };
 
-let number = document.getElementById("number1");
+let number1 = document.getElementById("number1");
 let counter = 0; 
 setInterval(() => {
     if(counter == 65){
@@ -25,6 +25,54 @@ setInterval(() => {
     }
     else{
     counter += 1;
-    number.innerHTML = counter + "%";
+    number1.innerHTML = counter + "%";
+    }
+}, 30);
+
+let number2 = document.getElementById("number2");
+let counter2 = 0; 
+setInterval(() => {
+    if(counter2 == 65){
+        clearInterval
+    }
+    else{
+    counter2 += 1;
+    number2.innerHTML = counter + "%";
+    }
+}, 30);
+
+let number3 = document.getElementById("number3");
+let counter3 = 0; 
+setInterval(() => {
+    if(counter3 == 65){
+        clearInterval
+    }
+    else{
+    counter3 += 1;
+    number3.innerHTML = counter + "%";
+    }
+}, 30);
+
+let number4 = document.getElementById("number4");
+let counter4 = 0; 
+setInterval(() => {
+    if(counter4 == 65){
+        clearInterval
+    }
+    else{
+    counter4 += 1;
+    number4.innerHTML = counter + "%";
+    }
+}, 30);
+
+let number5 = document.getElementById("number5");
+let counter5 = 0; 
+setInterval(() => {
+    if(counter5 == 65){
+        clearInterval
+    }
+    else{
+    counter5 += 1;
+    number5.innerHTML = counter + "%";
     }
 }, 30);
